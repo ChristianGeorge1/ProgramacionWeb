6ba07e477a2d0c13f766d73e9dac657e65bab5fd
20154901 CASTELLANOS GEORGE CHRISTIAN <20154901@TELEMATICA.UCOL.MX>
2020-03-13 07:41:24 -0600

    Actividad

--- 11_03_20/address/app.js
+++ 11_03_20/address/app.js
@@ -5,18 +5,21 @@ const address = require('./address.js');
 let num = randomNum(1, 88) //Un numero random
 let num2 = randomNum(1, 19);
 
+    
+request({url: urlAddress, json: true}, (error, response)=>{ 
+    var lat = response.body.results[0].locations[0].latLng.lat;
+    var lng = response.body.results[0].locations[0].latLng.lng;
+})
+
 const url = `https://swapi.co/api/people/${num}/` //Url de la api
-const urlTemperature = 'https://api.darksky.net/forecast/add880fee823c4a50da5b84764484216/19.2433,-103.725?lang=es'
+const urlTemperature = 'https://api.darksky.net/forecast/add880fee823c4a50da5b84764484216/' +lat+','+lng+'?lang=es' //19.2433,-103.725?lang=es'
 const urlAddress = 'http://www.mapquestapi.com/geocoding/v1/address?key=skDhBDWHK487c5vZTLcfwOtDHNyP19Ue&location='+address[num2].address.street;
 
 
 request({url: url,  json: true}, (error, response)=>{  //Metodo request para parsear mi objeto
     //console.log(response.body)
-    //console.log(`Name: ${response.body.name}`)
-    
-    request({url: urlAddress, json: true}, (error, response)=>{ 
-        console.log(response.body.results[0].providedLocation)
-    })
+    console.log(`Name: ${response.body.name}`)
+
 
 })
 //console.log()
